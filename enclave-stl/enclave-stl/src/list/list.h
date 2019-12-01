abc7af6ec2d1b11aa6af4fac81dd0e0f6e92438c
ArtemGord1728 <artemseraf01@mail.ru>
2019-12-01 22:51:32 +0300

    added some new functionality

--- enclave-stl/enclave-stl/src/list/list.h
+++ enclave-stl/enclave-stl/src/list/list.h
@@ -9,25 +9,43 @@ namespace enclave_stl
 	public:
 		list();
 
-		bool exist(const int index);
+		bool exist(const int index) noexcept;
 
-		bool exist(Type* element);
+		bool exist(Type* element) noexcept;
 
 		int is_empty();
 
-		void add(int index, Type* element);
+		void add(int index, const Type* element);
 
-		void add(Type element);
+		void push_back(const Type& value);
+
+		void add(const Type& element);
 
 		void remove(int index);
 
 		void remove(Type* element);
 
+		size_t size();
+
+		bool& operator<=(const list& other);
+
+		bool& operator>=(const list& other);
+
+		list& operator=(const list& other);
+
+		bool& operator==(const list& other);
+
+		bool& operator<(const list& other);
+
+		bool& operator>(const list& other);
+
+		void clear() noexcept;
+
 		virtual ~list();
 
 	private:
 		int _size = 10;
-		Type _data[];
+		Type _data;
 	};
 }
 
