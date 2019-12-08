2680789bf38496eec8e4be1ed8da78ac0457e3cb
ArtemGord1728 <artemseraf01@mail.ru>
2019-12-08 03:10:18 +0300

    rename extensions

--- enclave-stl/enclave-stl/src/algorithms/binary_search.h
+++ enclave-stl/enclave-stl/src/algorithms/binary_search.h
@@ -1,1 +1,25 @@
-#pragma once
+#ifndef ENCLAVE_STL_BINARY_SEARCH
+#define ENCLAVE_STL_BINARY_SEARCH
+
+#include <iostream>
+
+namespace enclave_stl
+{
+	namespace algorithms
+	{
+		class binary_search
+		{
+		public:
+			void search_element();
+			unsigned get_low_element();
+			unsigned get_high_element();
+
+		private:
+			unsigned int low_element = 0;
+			unsigned int high_element = 0;
+			bool sorted = false;
+		};
+	}
+}
+
+#endif
