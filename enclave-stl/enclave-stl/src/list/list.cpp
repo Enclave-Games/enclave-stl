934f9998e4956068b13ea17c8bb80b8f16116a3f
ArtemGord1728 <artemseraf01@mail.ru>
2019-12-08 03:26:55 +0300

    added some operators and methods

--- enclave-stl/enclave-stl/src/list/list.cpp
+++ enclave-stl/enclave-stl/src/list/list.cpp
@@ -6,25 +6,45 @@ namespace enclave_stl
 	template <typename Type>
 	list<Type>::list()
 	{
-		_size = 10;
-		_data = new Type[_size];
+		_size = 0;
+		elements = new Type[_size];
 	}
 
 	template <typename Type>
-	bool list<Type>::exist(Type* element)
+	void list<Type>::add(const Type& _element) // test implementation
 	{
-		return _data[_size] == element;
+		//elements[_size++] = _element;
+		_size++;
 	}
 
 	template <typename Type>
-	void list<Type>::add(const Type& element)
-	{
-		_data[_size++] = element;
-	}
-
-	template <typename Type>
-	size_t list<Type>::size()
+	unsigned int list<Type>::size()
 	{
 		return this->_size;
+	}
+
+	template <typename Type>
+	bool list<Type>::empty()
+	{
+		return this->_size == 0;
+	}
+
+
+	template <typename Type>
+	Type& list<Type>::operator[](const size_t index)
+	{
+		return elements[index];
+	}
+
+	template <typename Type>
+	bool& operator<(list<Type>& other1, list<Type>& other2)
+	{
+		return other1.size() < other2.size();
+	}
+
+	template <typename Type>
+	bool operator==(list<Type>& other1, list<Type>& other2)
+	{
+		return other1.size() == other2.size();
 	}
 }
--- enclave-stl/enclave-stl/src/list/list.h
+++ enclave-stl/enclave-stl/src/list/list.h
@@ -8,44 +8,15 @@ namespace enclave_stl
 	{
 	public:
 		list();
+		unsigned int size();
+		bool empty();
+		void add(const Type& _element);
 
-		bool exist(const int index) noexcept;
 
-		bool exist(Type* element) noexcept;
-
-		int is_empty();
-
-		void add(int index, const Type* element);
-
-		void push_back(const Type& value);
-
-		void add(const Type& element);
-
-		void remove(int index);
-
-		void remove(Type* element);
-
-		size_t size();
-
-		bool& operator<=(const list& other);
-
-		bool& operator>=(const list& other);
-
-		list& operator=(const list& other);
-
-		bool& operator==(const list& other);
-
-		bool& operator<(const list& other);
-
-		bool& operator>(const list& other);
-
-		void clear() noexcept;
-
-		virtual ~list();
-
+		Type& operator[](const size_t index);
 	private:
-		int _size = 10;
-		Type _data;
+		unsigned int _size;
+		Type* elements;
 	};
 }
 
