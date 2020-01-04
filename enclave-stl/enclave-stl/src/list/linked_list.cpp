7dafec9d794191fec9a7e9dac17b64dc6d770709
ArtemGord1728 <artemseraf01@mail.ru>
2020-01-04 02:06:36 +0200

    added defined methods and implementation for push()

--- enclave-stl/enclave-stl/src/list/linked_list.cpp
+++ enclave-stl/enclave-stl/src/list/linked_list.cpp
@@ -2,5 +2,60 @@
 
 namespace enclave_stl
 {
+	template <typename T>
+	linked_list<T>::linked_list()
+	{
+		size = 0;
+		head = new node;
+	}
 
+	template <typename T>
+	void linked_list<T>::push(T value)
+	{
+		node* temp_node = new node;
+		temp_node->next = nullptr;
+		temp_node->data = value;
+		temp_node->next = head->next;
+		head->next = temp_node->next;
+		++size;
+	}
+
+	template <typename T>
+	void linked_list<T>::pop()
+	{
+
+	}
+
+	template <typename T>
+	void linked_list<T>::remove(const T& value)
+	{
+
+	}
+
+	template <typename T>
+	void linked_list<T>::clear() noexcept
+	{
+
+	}
+
+	template <typename T>
+	size_t linked_list<T>::length()
+	{
+		return size;
+	}
+
+	template <typename T>
+	bool linked_list<T>::is_empty()
+	{
+		return size == 0;
+	}
+
+	template <typename T>
+	void linked_list<T>::print()
+	{
+		while (head != nullptr)
+		{
+			std::cout << head->data << std::endl;
+		}
+	}
 }
--- enclave-stl/enclave-stl/src/list/linked_list.h
+++ enclave-stl/enclave-stl/src/list/linked_list.h
@@ -1,19 +1,35 @@
 #ifndef ENCAVE_STL_LINKED_LIST
 #define ENCAVE_STL_LINKED_LIST
 
+#include <iostream>
+
 namespace enclave_stl
 {
+	template <typename T>
 	class linked_list
 	{
 	public:
-		typedef struct node
-		{
-			void* data;
-			struct node* next;
-		} node;
+		linked_list();
+		void push(T value);
+
+		void pop();
+		void remove(const T& value);
+		
+		void print();
+
+		void clear() noexcept;
+		bool is_empty();
+		size_t length();
 
 	private:
+		typedef struct node
+		{
+			T data;
+			node* next;
+		};
+
 		node* head;
+		size_t size;
 	};
 }
 
