ca7050abe4a78fc5dab35d455c48e22601723785
ArtemGord1728 <artemseraf01@mail.ru>
2020-03-15 04:00:27 +0300

    added some implementation for map

--- enclave-stl/enclave-stl/src/map/map.h
+++ enclave-stl/enclave-stl/src/map/map.h
@@ -1,9 +1,32 @@
 #ifndef ENCLAVE_STL_MAP
 #define ENCLAVE_STL_MAP
 
+#include <functional>
+#include <exception>
+
 namespace estl
 {
+	template<class Key, 
+		class T,
+		class Compare = std::less<Key>, 
+		class Allocator = std::allocator<std::pair<const Key, T>>>
+	class map
+	{
+	public:
+		typedef map<Key, T, Allocator>  this_type;
+		typedef Key						key_type;
+		typedef T						mapped_type;
+		typedef std::pair<const Key, T> value_type;
+		typedef size_t					size_type;
+		typedef ptrdiff_t				difference_type;
+		typedef Compare					compare_type;
+		typedef Allocator				allocator_type;
+		typedef value_type&		reference;
 
+		T& at(const Key& key);
+		const T& at(const Key& key) const;
+	protected:
+	};
 }
 
 #endif
