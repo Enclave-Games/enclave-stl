85cc16d5d1b0ad5925372d4b73fd533f1b26b595
ArtemGord1728 <artemseraf01@mail.ru>
2020-03-19 00:11:25 +0300

    added new mechanisms

--- enclave-stl/enclave-stl/include/base/config.h
+++ enclave-stl/enclave-stl/include/base/config.h
@@ -1,6 +1,17 @@
 #ifndef ENCLAVE_STL_CONFIG
 #define ENCLAVE_STL_CONFIG
 
+///////////////////////////////////////////////////////////////////////////////
+/// This is the ENCLAVE_STL configuration file. All configurable parameters of ENCLAVE_STL
+/// are controlled through this file. However, all the settings here can be
+/// manually overridden by the user
+//
+///////////////////////////////////////////////////////////////////////////////
 
+#define ENCLAVE_ASSERT(expression) static_assert(expression, #expression)
+
+#define ENCLAVE_ASSERT_MSG(expression, message) static_assert(expression, message)
+
+typedef size_t SIZE_T;
 
 #endif
