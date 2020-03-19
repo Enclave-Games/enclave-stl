3bab92be4775f1186f5e40b015acb0b219caa52a
ArtemGord1728 <artemseraf01@mail.ru>
2020-03-20 00:57:14 +0300

    added move() and refactored code

--- enclave-stl/enclave-stl/include/algorithms.h
+++ enclave-stl/enclave-stl/include/algorithms.h
@@ -434,14 +434,18 @@ namespace ealg
 	}
 
 	template< class T >
-	T&& forward(typename std::remove_reference<T>::type& t) noexcept
+	T&& forward(typename std::remove_reference<T>::type& t) ENCLAVE_NOEXC
 	{
 		ENCLAVE_ASSERT_MSG(!is_lvalue_reference<T>::value,
 			"can not forward an rvalue as an lvalue");
 		return static_cast<T&&>(t);
 	}
 	
-
+	template< class T >
+	typename std::remove_reference<T>::type&& move(T&& t) ENCLAVE_NOEXC
+	{
+		return static_cast<typename std::remove_reference<T>::type&&>(t);
+	}
 
 	template< class RandomIt >
 	void random_shuffle(RandomIt first, RandomIt last)
