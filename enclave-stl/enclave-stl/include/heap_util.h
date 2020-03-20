033e0e407d264a7917dfe1d7755d99bb0f5821f0
ArtemGord1728 <artemseraf01@mail.ru>
2020-03-21 02:42:02 +0300

    added heap utility

---
+++ enclave-stl/enclave-stl/include/heap_util.h
@@ -0,0 +1,27 @@
+#ifndef ENCLAVE_STL_HEAP_UTIL
+#define ENCLAVE_STL_HEAP_UTIL
+
+#include "base/config.h"
+
+namespace eheap_util
+{
+	template< class RandomIt >
+	bool is_heap(RandomIt first, RandomIt last)
+	{
+
+	}
+
+	template< class RandomIt >
+	RandomIt is_heap_until(RandomIt first, RandomIt last)
+	{
+
+	}
+
+	template< class RandomIt, class Compare >
+	RandomIt is_heap_until(RandomIt first, RandomIt last, Compare comp)
+	{
+
+	}
+}
+
+#endif
