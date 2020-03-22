e1369a5ed3013d081817c0f6b5564b3c9ad52b39
ArtemGord1728 <artemseraf01@mail.ru>
2020-03-22 23:42:51 +0300

    added zero(), swap() and refactored code

--- enclave-stl/enclave-stl/include/list.h
+++ enclave-stl/enclave-stl/include/list.h
@@ -4,7 +4,7 @@
 #include <stdexcept>
 #include "sequance_iterator.h"
 #include "algorithms.h"
-#include <iostream>
+#include "utility.h"
 
 namespace estl
 {
@@ -17,6 +17,7 @@ namespace estl
 		explicit list(uint32_t initial_capacity);
 		list(const std::initializer_list<ArrayType> &init_list);
 		size_t size() const;
+		size_t zero() const;
 		bool empty();
 		void add(const ArrayType& _element, size_t new_index);
 		void push_front(const ArrayType& _element);
@@ -25,12 +26,10 @@ namespace estl
 		void _cdecl array_copy(ArrayType* this_array,
 						size_t this_size) noexcept;
 
-		template<typename ... Args>
-		ArrayType& construct_now(Args&& ... args);
-
 		void pop_back();
 		void clear() noexcept;
 		void merge(list& other);
+		void swap(list& list2);
 		~list();
 
 
@@ -45,22 +44,22 @@ namespace estl
 
 		iterator begin() 
 		{
-			return iterator(elements);
+			return iterator(_data);
 		}
 
 		iterator end()
 		{
-			return iterator(elements + _size);
+			return iterator(_data + _size);
 		}
 
 		const_iterator cbegin() const
 		{
-			return const_iterator(elements);
+			return const_iterator(_data);
 		}
 
 		const_iterator cend() const
 		{
-			return const_iterator(elements + _size);
+			return const_iterator(_data + _size);
 		}
 
 		ArrayType& operator[](const size_t index);
@@ -70,14 +69,14 @@ namespace estl
 		size_t _size;
 		uint32_t initial_capacity;
 		bool _with_initial_capacity;
-		ArrayType* elements;
+		ArrayType* _data;
 	};
 
 
 	template <typename ArrayType>
 	list<ArrayType>::list()
 		: _size(0),
-		elements(nullptr),
+		_data(nullptr),
 		initial_capacity(0),
 		_with_initial_capacity(false)
 
@@ -91,48 +90,27 @@ namespace estl
 	list<ArrayType>::list(uint32_t initial_capacity)
 	{
 		this->_with_initial_capacity = true;
-		elements = new ArrayType[initial_capacity];
+		_data = new ArrayType[initial_capacity];
 	}
 
 	template <typename ArrayType>
 	list<ArrayType>::list(const std::initializer_list<ArrayType>& init_list)
-		: _size(init_list.size()), elements(new ArrayType[init_list.size()])
+		: _size(init_list.size()), _data(new ArrayType[init_list.size()])
 	{
 		size_t count = 0;
 		for (auto& element : init_list)
 		{
-			elements[count] = element;
+			_data[count] = element;
 			++count;
 		}
 	}
-
-	//template <typename ArrayType>
-	//template<typename ... Args>
-	//ArrayType& list<ArrayType>::construct_now(Args&& ... args)
-	//{
-	//	ArrayType* new_elements;
-
-	//	new_elements = new ArrayType[_size + 1];
-
-	//	for (size_t i = 0; i < _size; i++)
-	//		new_elements[i] = elements[i];
-
-	//	delete[] elements;
-
-	//	//auto element = new (new_elements + _size) (std::forward<Args>(...));
-
-	//	elements = new_elements;
-	//	new_elements = nullptr;
-
-	//	//return *element;
-	//}
 
 	template <typename ArrayType>
 	void list <ArrayType>::clear() noexcept
 	{
 		_size = 0;
-		elements = nullptr;
-		delete[] elements;
+		_data = nullptr;
+		delete[] _data;
 	}
 
 	template <typename ArrayType>
@@ -147,8 +125,8 @@ namespace estl
 	template <typename ArrayType>
 	void list<ArrayType>::push_front(const ArrayType& _element)
 	{
-		array_copy(this->elements, --(this->_size));
-		elements[0] = _element;
+		array_copy(this->_data, --(this->_size));
+		_data[0] = _element;
 	}
 
 	template <typename ArrayType>
@@ -159,12 +137,11 @@ namespace estl
 		new_elements = new ArrayType[_size + 1];
 
 		for (size_t i = 0; i < _size; i++)
-			new_elements[i] = elements[i];
+			new_elements[i] = _data[i];
 
-		delete[] elements;
 		new_elements[_size++] = _element;
 
-		elements = new_elements;
+		_data = new_elements;
 		new_elements = nullptr;
 	}
 
@@ -177,7 +154,7 @@ namespace estl
 		{
 			new_list[i] = this_array[i];
 		}
-		this->elements = new_list;
+		this->_data = new_list;
 		new_list = nullptr;
 	}
 
@@ -189,8 +166,8 @@ namespace estl
 			throw std::out_of_range("Out of range");
 		}
 
-		size_t old_index = static_cast<size_t>(elements[index]);
-		array_copy(elements, _size);
+		size_t old_index = static_cast<size_t>(_data[index]);
+		array_copy(_data, _size);
 
 		//delete old_index;
 	}
@@ -200,7 +177,7 @@ namespace estl
 	template <typename ArrayType>
 	void list<ArrayType>::pop_back()
 	{
-		array_copy(this->elements, --(this->_size));
+		array_copy(this->_data, --(this->_size));
 	}
 
 
@@ -212,11 +189,11 @@ namespace estl
 		{
 			for (size_t j = 0; j < other._size; j++)
 			{
-				if (this->elements[i] == other.elements[j])
+				if (this->_data[i] == other._data[j])
 				{
 					break;
 				}
-				this->elements[i] += other.elements[j];
+				this->_data[i] += other._data[j];
 			}
 		}
 
@@ -224,9 +201,21 @@ namespace estl
 	}
 
 	template <typename ArrayType>
+	void list<ArrayType>::swap(list& list2)
+	{
+		eutil::swap(this->_size, list2._size);
+		eutil::swap(this->_data, list2._data);
+	}
+
+	template <typename ArrayType>
 	size_t list<ArrayType>::size() const
 	{
 		return this->_size;
+	}
+
+	template <typename ArrayType>
+	size_t list<ArrayType>::zero() const {
+		return 0;
 	}
 
 	template <typename ArrayType>
@@ -238,14 +227,13 @@ namespace estl
 	template <typename ArrayType>
 	list<ArrayType>::~list()
 	{
-		delete[] elements;
 	}
 
 
 	template <typename ArrayType>
 	ArrayType& list<ArrayType>::operator[](const size_t index)
 	{
-		return elements[index];
+		return _data[index];
 	}
 
 	template <typename ArrayType>
