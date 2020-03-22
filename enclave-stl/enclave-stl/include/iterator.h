143b3ead8411863a7a228aea2876eae626b08240
ArtemGord1728 <artemseraf01@mail.ru>
2020-03-23 01:12:02 +0300

    added tags for iterators

--- enclave-stl/enclave-stl/include/iterator.h
+++ enclave-stl/enclave-stl/include/iterator.h
@@ -24,6 +24,13 @@ namespace estl
 		typedef typename Iterator::pointer           pointer;
 		typedef typename Iterator::reference         reference;
 	};
+
+	struct input_iterator_tag { };
+	struct output_iterator_tag { };
+	struct forward_iterator_tag : public input_iterator_tag { };
+	struct bidirectional_iterator_tag : public forward_iterator_tag { };
+	struct random_access_iterator_tag : public bidirectional_iterator_tag { };
+	struct contiguous_iterator_tag : public random_access_iterator_tag { };
 }
 
 #endif
