656747ab976f428f056c8d3e4b24278955ed4719
ArtemGord1728 <artemseraf01@mail.ru>
2020-04-06 13:35:50 +0300

    defined methods

--- enclave-stl/enclave-stl/include/sort_extra.h
+++ enclave-stl/enclave-stl/include/sort_extra.h
@@ -1,9 +1,33 @@
 #ifndef ENCLAVE_STL_SORT_EXTRA
 #define ENCLAVE_STL_SORT_EXTRA
 
+#include <algorithm>
+
 namespace ealg_sort
 {
+	template< class RandomIt >
+	void stable_sort(RandomIt first, RandomIt last)
+	{
 
+	}
+
+	template< class RandomIt, class Compare >
+	void stable_sort(RandomIt first, RandomIt last, Compare comp)
+	{
+
+	}
+
+	template< class RandomIt >
+	void partial_sort(RandomIt first, RandomIt middle, RandomIt last)
+	{
+
+	}
+
+	template< class RandomIt, class Compare >
+	void partial_sort(RandomIt first, RandomIt middle, RandomIt last, Compare comp)
+	{
+
+	}
 }
 
 #endif
