f5b4af4c7fe8682426f1b04c1551d7bb7cfc59e6
ArtemGord1728 <artemseraf01@mail.ru>
2020-04-18 01:49:01 +0300

    refactored code

--- enclave-stl/enclave-stl/include/sort_extra.h
+++ enclave-stl/enclave-stl/include/sort_extra.h
@@ -3,24 +3,30 @@
 
 #include <algorithm>
 
-namespace ealg_sort
+namespace esort
 {
 	template< class RandomIt >
 	void stable_sort(RandomIt first, RandomIt last)
 	{
-
+		return esort::stable_sort(first, last, std::less<>);
 	}
 
 	template< class RandomIt, class Compare >
 	void stable_sort(RandomIt first, RandomIt last, Compare comp)
 	{
+		if (first != last)
+		{
+			for (RandomIt start = first; start != last;)
+			{
 
+			}
+		}
 	}
 
 	template< class RandomIt >
 	void partial_sort(RandomIt first, RandomIt middle, RandomIt last)
 	{
-
+		return partial_sort(first, last, std::less<>);
 	}
 
 	template< class RandomIt, class Compare >
