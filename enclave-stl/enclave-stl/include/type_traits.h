58051ea2e1192a9f735bc53f11314e6c072aa933
ArtemGord1728 <artemseraf01@mail.ru>
2020-04-18 02:35:34 +0300

    added new structs

--- enclave-stl/enclave-stl/include/type_traits.h
+++ enclave-stl/enclave-stl/include/type_traits.h
@@ -1,9 +1,26 @@
 #ifndef ENCLAVE_STL_TYPE_TRAITS
 #define ENCLAVE_STL_TYPE_TRAITS
 
-namespace estl
+namespace etypes
 {
+	template< class T, T v >
+	struct integral_constant
+	{
+		static constexpr T value = v;
+		using value_type = T;
+		using type = integral_constant;
+	};
 
+	template< class T >
+	struct is_void
+	{
+
+	};
+
+	template< class T, class U >
+	struct is_same 
+	{
+	};
 }
 
 #endif
