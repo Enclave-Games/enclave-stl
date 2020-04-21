86d1553c57fa63672bcd31ea51b4a1f70be8314d
ArtemGord1728 <artemseraf01@mail.ru>
2020-04-22 00:29:50 +0300

    added new file

---
+++ enclave-stl/enclave-stl/include/sorted_set.h
@@ -0,0 +1,20 @@
+#ifndef ENCLAVE_STL_SORTED_SET
+#define ENCLAVE_STL_SORTED_SET
+
+#include "base/config.h"
+#include "base/functors.h"
+
+namespace estl
+{
+	template < class Type, 
+			class Compare = efun::less<Type>()>
+	class sorted_set
+	{
+	public:
+	
+	private:
+
+	};
+}
+
+#endif
