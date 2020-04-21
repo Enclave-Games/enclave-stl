f15fbe276db900f9dd8aad33f6abeae1e63a06c6
ArtemGord1728 <artemseraf01@mail.ru>
2020-04-22 00:34:33 +0300

    added new usebable functors

--- enclave-stl/enclave-stl/include/base/functors.h
+++ enclave-stl/enclave-stl/include/base/functors.h
@@ -1,12 +1,14 @@
 #ifndef ENCLAVE_STL_FUNCTORS
 #define ENCLAVE_STL_FUNCTORS
 
+#include "base/config.h"
+
 namespace efun
 {
 	template <typename _Ty = void>
 	struct less
 	{
-		constexpr bool operator()(_Ty&& left, _Ty&& right) const
+		ENCLAVE_CONSTEXPR bool operator()(_Ty&& left, _Ty&& right) const
 		{
 			return left < right;
 		}
@@ -16,9 +18,104 @@ namespace efun
 	struct less<void>
 	{
 		template <class _Ty1, class _Ty2>
-		constexpr bool operator()(_Ty1&& left, _Ty2&& right) const
+		ENCLAVE_CONSTEXPR bool operator()(_Ty1&& left, _Ty2&& right) const
 		{
 			return left < right;
+		}
+	};
+
+	template< class T = void >
+	struct equal_to
+	{
+		ENCLAVE_CONSTEXPR bool operator()(T&& left, T&& right) const
+		{
+			return left == right;
+		}
+	};
+
+	template<>
+	struct equal_to<void>
+	{
+		template <class _Ty1, class _Ty2>
+		ENCLAVE_CONSTEXPR bool operator()(_Ty1&& left, _Ty2&& right) const
+		{
+			return left == right;
+		}
+	};
+
+	template< class T = void >
+	struct not_equal_to
+	{
+		ENCLAVE_CONSTEXPR bool operator()(T&& left, T&& right) const
+		{
+			return left != right;
+		}
+	};
+
+	template<>
+	struct not_equal_to<void>
+	{
+		template <class _Ty1, class _Ty2>
+		ENCLAVE_CONSTEXPR bool operator()(_Ty1&& left, _Ty2&& right) const
+		{
+			return left != right;
+		}
+	};
+
+	template< class T = void >
+	struct greater
+	{
+		ENCLAVE_CONSTEXPR bool operator()(T&& left, T&& right) const
+		{
+			return left > right;
+		}
+	};
+
+	template<>
+	struct greater<void>
+	{
+		template <class _Ty1, class _Ty2>
+		ENCLAVE_CONSTEXPR bool operator()(_Ty1&& left, _Ty2&& right) const
+		{
+			return left > right;
+		}
+	};
+
+	template< class T = void >
+	struct greater_equal
+	{
+		ENCLAVE_CONSTEXPR bool operator()(T&& left, T&& right) const
+		{
+			return left >= right;
+		}
+	};
+
+	template<>
+	struct greater_equal<void>
+	{
+		template <class _Ty1, class _Ty2>
+		ENCLAVE_CONSTEXPR bool operator()(_Ty1&& left, _Ty2&& right) const
+		{
+			return left >= right;
+		}
+	};
+
+	template < typename _Ty = void >
+	struct less_equal
+	{
+		ENCLAVE_CONSTEXPR bool operator()(_Ty&& left, _Ty&& right) const
+		{
+			return left <= right;
+		}
+	};
+
+	template <>
+	struct less_equal<void>
+	{
+		template <class _Ty1, class _Ty2>
+		ENCLAVE_CONSTEXPR bool operator()(_Ty1&& left, _Ty2&& right) const
+		{
+			return left <= right;
 		}
 	};
 }
