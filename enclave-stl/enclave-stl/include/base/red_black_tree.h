3c3bf668c312a1d611ee2cfca449cbdc2bea26a7
ArtemGord1728 <artemseraf01@mail.ru>
2020-04-26 00:29:50 +0300

    added some structs for red black tree

--- enclave-stl/enclave-stl/include/base/red_black_tree.h
+++ enclave-stl/enclave-stl/include/base/red_black_tree.h
@@ -3,7 +3,38 @@
 
 namespace estl
 {
-	class red_black_tree
+	enum rb_tree_color
+	{
+		RB_TREE_COLOR_RED,
+		RB_TREE_COLOR_BLACK
+	};
+
+	enum rb_tree_side
+	{
+		RB_TREE_SIDE_LEFT,
+		RB_TREE_SIDE_RIGHT
+	};
+
+	struct rbtree_node_base
+	{
+		using this_type = rbtree_node_base;
+
+	public:
+		this_type* mpNodeRight;
+		this_type* mpNodeLeft;
+		this_type* mpNodeParent;
+		char mColor;
+	};
+
+	template <typename Value>
+	struct rbtree_node : public rbtree_node_base
+	{
+		Value mValue;
+
+		rbtree_node(const rbtree_node&) = delete;
+	};
+
+	class rb_tree
 	{
 	public:
 
