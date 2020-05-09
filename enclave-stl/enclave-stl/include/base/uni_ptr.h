88550f1295f9a0f531b5a01d950a2cc8e2dc76d2
ArtemGord1728 <artemgord00@gmail.com>
2020-05-10 00:27:04 +0300

    updated files

---
+++ enclave-stl/enclave-stl/include/base/uni_ptr.h
@@ -0,0 +1,15 @@
+#ifndef ENCLAVE_STL_SMART_PTR
+#define ENCLAVE_STL_SMART_PTR
+
+
+template <class T>
+class UniPtr
+{
+public:
+
+
+private:
+	T* _ptr;
+};
+
+#endif
---
+++ enclave-stl/enclave-stl/include/deque.h
@@ -0,0 +1,11 @@
+#ifndef ENCLAVE_STL_DEQUE
+#define ENCLAVE_STL_DEQUE
+
+
+namespace estl
+{
+
+}
+
+
+#endif
